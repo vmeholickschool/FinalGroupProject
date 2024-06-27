11dabfdde3494333cfce5135bbcc90792ab691fb
Zack Prohonic <zackprohonic20@gmail.com>
2024-06-27 19:20:02 -0400

    Scaffolded VS and created SQL Database

---
+++ final-project-career-hub-app/final-project-career-hub-app/Models/Job.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace final_project_career_hub_app.Models;
+
+public partial class Job
+{
+    public int JobId { get; set; }
+
+    public string? JobTitle { get; set; }
+
+    public string? CompanyName { get; set; }
+
+    public string? City { get; set; }
+
+    public string? State { get; set; }
+
+    public string? ZipCode { get; set; }
+
+    public string? SalaryRange { get; set; }
+
+    public string? JobDescription { get; set; }
+
+    public string? ExperienceLevel { get; set; }
+
+    public virtual ICollection<ApplicationStatus> ApplicationStatuses { get; set; } = new List<ApplicationStatus>();
+
+    public virtual ICollection<SavedJob> SavedJobs { get; set; } = new List<SavedJob>();
+}
---
+++ final-project-career-hub-app/final-project-career-hub-app/Models/SavedJob.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace final_project_career_hub_app.Models;
+
+public partial class SavedJob
+{
+    public int SaveId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? JobId { get; set; }
+
+    public virtual Job? Job { get; set; }
+}
